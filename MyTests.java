d41f6316a1c1911acce76ffe2f45cc8ae97b7ae1
RodneyNickles <36527686+RodneyNickles@users.noreply.github.com>
2019-04-08 18:43:37 -0400

    MyTests class for birdgame

---
+++ MyTests.java
@@ -0,0 +1,57 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MyTests.
+ */
+public class MyTests {
+	
+	@Test
+	public void ScoreboardTest() {
+		Scoreboard test = new Scoreboard();
+		assertEquals(test.hashCode(), test.hashCode());
+		assertEquals(test.toString(), test.toString());
+		assertEquals(test.getClass(), test.getClass());
+		}
+	
+	@Test
+	public void StickTest() {
+		Stick test = new Stick();
+		assertEquals(test.xPos, 0);
+		assertEquals(test.yPos, 0);
+		assertEquals(test.xSize, 0);
+		assertEquals(test.ySize, 0);
+		assertEquals(test.checkOnPile(), true);
+		assertEquals(test.fall(), true);
+		assertEquals(test.collided(), true);
+	}
+	
+	@Test
+	public void TimerTest() {
+		Timer test = new Timer();
+		assertEquals(test.hashCode(), test.hashCode());
+		assertEquals(test.getClass(), test.getClass());
+	}
+	
+    @Test
+    public void ViewTest() {
+    	View test = new View();
+    	assertEquals(test.getClass(), test.getClass());
+    	assertEquals(test.hashCode(), test.hashCode());
+    	assertEquals(test.equals(test), true);
+    
+    	}
+    
+    @Test
+    public void WindGustTest() {
+    	WindGust tester = new WindGust();
+    	assertEquals(tester.isCollided(), true);
+    	assertEquals(tester.isForward, false);
+    	assertEquals(tester.toString(), tester.toString());
+    }
+
+}

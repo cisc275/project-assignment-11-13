2a5f662aaffb9f27a37dbf9e911e1d23c6153be6
Drew McQuiston <drwmcq@aol.com>
2019-04-14 18:44:06 -0400

    Added main, updated constructor to open blank window so that key listeners work, addded delay to update() method.

--- src/View.java
+++ src/View.java
@@ -1,12 +1,50 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
-public class View {
+public class View extends JPanel {
     PlayableBird bird;
     JFrame frame = new JFrame();
+    final static int frameWidth = 600;
+    final static int frameHeight = 400;
+    
+    public static void main(String[] args) {
+        Controller control = new Controller();
+        control.start();
+    }
+    
+    public int getWidth() {
+        return frameWidth;
+    }
 
-    public View(){}
+    public int getHeight() {
+        return frameHeight;
+    }
+    
+    public View(){
+    	frame.getContentPane().add(this);
+        frame.setBackground(Color.gray);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(frameWidth, frameHeight);
+        frame.setVisible(true);
+        
+        frame.setFocusable(true);
+    }
 
-    public void update(){}
+    public void update(){
+    	//frame.repaint();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void createImage(){}
 

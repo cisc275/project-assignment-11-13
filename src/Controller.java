a1e5ecb09a01a2ffde45fa5fa4c6046e4a15913c
Drew McQuiston <drwmcq@aol.com>
2019-04-14 18:46:39 -0400

    Added key listeners in constuctor which tell the bird to move up or down, updated start() method.

--- src/Controller.java
+++ src/Controller.java
@@ -7,9 +7,44 @@ public class Controller implements ActionListener, KeyListener {
     View view;
     Model model;
 
-    public Controller(){}
+    public Controller(){
+    	view = new View();
+    	model = new Model(view.getWidth(), view.getHeight());
+    	
+    	view.frame.addKeyListener(new KeyListener() {
+        	public void keyPressed(KeyEvent e) {
+        		if (e.getKeyCode() == KeyEvent.VK_UP) {
+        			model.mainBird.upPressed = true; //Probably shouldn't call the bird's attributes like this directly, right? how should we change it?
+        		}
+        		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        			model.mainBird.downPressed = true; //Probably shouldn't call the bird's attributes like this directly, right? how should we change it?
+        		}
+        	}
 
-    public void start(){}
+			@Override
+			public void keyTyped(KeyEvent e) {
+				//In place just to have all required methods
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_UP) {
+					model.mainBird.upPressed = false;
+	    		}
+	    		else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+	    			model.mainBird.downPressed = false;
+	    		}
+			}
+        });
+    }
+
+    public void start(){
+    	for (int i = 0; i < 100; i++) {
+    		model.update();
+    		
+    		view.update();
+    	}
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -23,11 +58,9 @@ public class Controller implements ActionListener, KeyListener {
 
     @Override
     public void keyPressed(KeyEvent e) {
-
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
     }
 }

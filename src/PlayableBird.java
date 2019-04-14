5b6e44c3520a3c794bbaf2c39ca59e41709d7296
Drew McQuiston <drwmcq@aol.com>
2019-04-14 18:40:35 -0400

    Added yIncr and booleans to indicate key presses, and modified move() to update yPos accordingly.

--- src/PlayableBird.java
+++ src/PlayableBird.java
@@ -1,11 +1,22 @@
 public class PlayableBird extends Animal {
+	final int yIncr = 5; //Just made up this value for now
+	boolean upPressed = false;
+	boolean downPressed = false;
 
     public PlayableBird(String picFile, String name){
         super(picFile, name);
     }
 
     public boolean move(){
-        return true;
+    	//This method should only deal with up and down, right? Because the bird doesn't move left/right, the background and everything else does. 
+    	if (upPressed) {
+    		yPos -= yIncr; 
+    	}
+    	if (downPressed) {
+    		yPos += yIncr;
+    	}
+    	System.out.println("Bird's y-position: " + yPos); //For testing to see that value was updating
+    	return true;
     }
 
     public boolean dive(){

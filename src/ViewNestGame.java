18945635f8b63c866bbc2ac3879a16e76718c7e3
tspatt <tspatt@udel.edu>
2019-05-06 13:38:16 -0500

    Updated stick datastructure

--- src/ViewNestGame.java
+++ src/ViewNestGame.java
@@ -9,10 +9,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+<<<<<<< refs/remotes/origin/TommysBranch
 public class ViewNestGame extends View  {
+=======
+public class ViewNestGame extends JPanel {
+>>>>>>> local
 
     static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-    JFrame frame = new JFrame();
+    //JFrame frame = new JFrame();
     final static int frameWidth = (int) screenSize.getWidth();
     final static int frameHeight = (int) screenSize.getHeight();
     String name;
@@ -38,6 +42,8 @@ public class ViewNestGame extends View  {
     }
 /*
     public ViewNestGame(){
+    	
+    /*
         frame.getContentPane().add(this);
         frame.setBackground(Color.cyan);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,19 +51,40 @@ public class ViewNestGame extends View  {
         frame.setUndecorated(true);
         frame.setVisible(true);
         frame.setFocusable(true);
+<<<<<<< refs/remotes/origin/TommysBranch
     }
 */
     public void addObjects(DropBird dropBird) {
         this.dropBird = dropBird;
         stickImg = createImage(this.dropBird.stickList.getFirst().picFile);
         
+=======
+*/
+    }
+
+    
+    public void addObjects(DropBird dropBird, Stick stickOne, Stick stickTwo, Stick stickThree) {
+        this.dropBird = dropBird;
+        stickImg = createImage(this.dropBird.stickList.getFirst().picFile);
+>>>>>>> local
         dropBirdImg = createImage(this.dropBird.picFile);
         return;
     }
 
+<<<<<<< refs/remotes/origin/TommysBranch
     public void update(DropBird dropBird){
         this.dropBird = dropBird;
         frame.repaint();
+=======
+    public void update(int x, int y, DropBird dropBird){
+        this.dropBird = dropBird;
+        //this.xloc = x;
+        //this.yloc = y;
+        //this.picFile = bird.picFile;
+        //this.name = bird.name;
+
+        this.repaint();
+>>>>>>> local
         try {
             Thread.sleep(33);
         } catch (InterruptedException e) {
@@ -79,9 +106,17 @@ public class ViewNestGame extends View  {
 
 
     public void paint(Graphics g){
+<<<<<<< refs/remotes/origin/TommysBranch
         g.drawImage(dropBirdImg, dropBird.getxPos(), dropBird.getyPos(), Color.cyan, this);
         for(Stick s: dropBird.stickList) {
         	g.drawImage(stickImg, s.getxPos(), s.getyPos(), Color.cyan, this);
         }
     }
+=======
+    	g.drawImage(dropBirdImg, dropBird.getxPos(), dropBird.getyPos(), Color.cyan, this);
+        for(Stick s: dropBird.stickList) {
+        	g.drawImage(stickImg, s.getxPos(), s.getyPos(), Color.cyan, this);
+        }
+}
+>>>>>>> local
 }

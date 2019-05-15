fbc713c752a0825fa19ad2e3f792e6ca451d504e
RodneyNickles <36527686+RodneyNickles@users.noreply.github.com>
2019-05-15 16:46:54 -0400

    scaled background for first game

--- src/Background.java
+++ src/Background.java
@@ -1,8 +1,11 @@
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class Background extends EnvironmentObject {
 
@@ -11,7 +14,22 @@ public class Background extends EnvironmentObject {
 	
     public Background(String picFile){
     	this.picFile = picFile;
-    	this.image = this.createImage(picFile);
+       	this.image = scaleImage(View.frameWidth,View.frameHeight, picFile);
+        //this.image = this.createImage(picFile);
+    }
+	public static BufferedImage scaleImage(int WIDTH, int HEIGHT, String filename) {
+        BufferedImage bi = null;
+        try {
+            ImageIcon ii = new ImageIcon(filename);//path to image
+            bi = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+            Graphics2D g2d = (Graphics2D) bi.createGraphics();
+            g2d.addRenderingHints(new RenderingHints(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY));
+            g2d.drawImage(ii.getImage(), 0, 0, WIDTH, HEIGHT, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return bi;
     }
     
 	public BufferedImage createImage(String path) {

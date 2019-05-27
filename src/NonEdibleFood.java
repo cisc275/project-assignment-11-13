206f8c87d853bb232a406485c065fa22b0dc9ead
charleslambert98 <clambert@udel.edu>
2019-05-27 13:04:04 -0400

    Documented NonEdibleFood

--- src/NonEdibleFood.java
+++ src/NonEdibleFood.java
@@ -4,6 +4,12 @@ public class NonEdibleFood extends Food {
 	private static final int NONEDIBLEFOOD_YSIZE = 40;
 	private static final int NONEDIBLEFOOD_POINTVALUE = -3;
 	
+	/**
+	 * Constructor for NonEdibleFood; initializes all class attributes
+	 * 
+	 * @param frameWidth
+	 * @param frameHeight
+	 */
 	public NonEdibleFood(int frameWidth, int frameHeight) {
 		super(frameWidth, frameHeight, NONEDIBLEFOOD_POINTVALUE);
 		xSize = NONEDIBLEFOOD_XSIZE;
@@ -11,10 +17,20 @@ public class NonEdibleFood extends Food {
 		image = this.createImage("ProjectPics/trash.png");
 	}
 
+	/**
+	 * Move function for the non-edible food
+	 * 
+	 * @return boolean
+	 */
     public boolean move(){
         return true;
     }
-
+    
+    /**
+     * Provides a template function for whether or not the non-edible food has collided with the playable bird
+     * 
+     * @return boolean
+     */
     public boolean collected(){
         return super.isCollected;
     }
